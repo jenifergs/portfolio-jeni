67da02a04012bb1bc74e1784fd695c687ae2bd76
Jenny Gonçalves <jeniferdeveloper@gmail.com>
2025-02-18 19:09:12 -0300

    change photo hero

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ function About() {
     <section id="sobre" className="about">
       <div className="container">
         <h2>Sobre Mim</h2>
+        <hr />
         <p>
           Meu nome é Jenifer Gonçalves, sou uma desenvolvedora Full Stack com experiência em React, Node.js e desenvolvimento de aplicações web e mobile. Tenho paixão por criar soluções eficazes e elegantes para problemas complexos.
         </p>
